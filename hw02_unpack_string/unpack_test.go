1ee2fe1e9839d2b1e1fe6cda44986691a36b0b86
merkulova.i <merkulova.i@selectel.ru>
2021-12-07 00:47:35 +0300

    HW2 is completed

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,9 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "AA2b", expected: "AAAb"},
+		{input: ".,?<", expected: ".,?<"},
+		{input: "g g1", expected: "g g"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
